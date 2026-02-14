0328f1dffa48b885fb422a7984bdb9eba6b93020
Navneet Kumar Jha <navneetkumarjha088@gmail.com>
2026-02-14 15:22:20 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import numpy as np
+import pickle
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+
+
+# Load the trained model and tokenizer
+model = load_model('lstm_model.h5')
+#lode the tokenizer
+with open('tokenizer.pickle', 'rb') as handle:
+    tokenizer = pickle.load(handle)
+# Function to predict the next word
+def predict_next_word(model, tokenizer, text, max_sequence_len):
+    sequence = tokenizer.texts_to_sequences([text])[0]
+    sequence = pad_sequences([sequence], maxlen=max_sequence_len-1, padding='pre')
+    predicted = model.predict(sequence, verbose=0)
+    predicted_word_index = np.argmax(predicted)
+    for word, index in tokenizer.word_index.items():
+        if index == predicted_word_index:
+            return word
+    return None
+
+# Streamlit app
+st.title("LSTM Next Word Prediction")
+input_text = st.text_input("Enter a sequence of words:")
+if st.button("Predict Next Word"):  
+    max_sequence_len = model.input_shape[1] + 1
+    next_word = predict_next_word(model, tokenizer, input_text, max_sequence_len)
+    st.write(f"Predicted Next Word: {next_word}")
